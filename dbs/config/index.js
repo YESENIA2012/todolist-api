da7ae2fe3565f2d59909d6d638ea39717b274e77
YESENIA2012 <gyesenia173@gmail.com>
2023-06-15 19:03:03 -0500

    implement migrations

--- dbs/config/index.js
+++ dbs/config/index.js
@@ -1,11 +1,13 @@
 import "../../dotenv.js";
 
+const { DB_USER, DB_PASS, DB_NAME, SERVER_HOST, DIALET } = process.env;
+
 export default {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.SERVER_HOST,
-    dialect: process.env.DIALET,
+    username: DB_USER,
+    password: DB_PASS,
+    database: DB_NAME,
+    host: SERVER_HOST,
+    dialect: DIALET,
   },
 };
